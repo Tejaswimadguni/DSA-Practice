4a6c4cb9abbb2a0a2aca5f7eaa6954250605571c
TEJASWI NAGARAJ MADGUNI <tmadguni@gmail.com>
2026-03-30 23:36:16 +0530

    Add solution for Ugly Number problem
    Implement a solution to determine if a number is ugly.

---
+++ Math/ugly_number.cpp
@@ -0,0 +1,18 @@
+// Problem: Ugly Number
+// Link: https://leetcode.com/problems/ugly-number/
+// Difficulty: Easy
+// Language: C++
+
+class Solution {
+public:
+    bool isUgly(int n) {
+
+        if(n<=0) return false;
+
+        while(n%2==0) n=n/2;
+        while(n%3==0) n=n/3;
+        while(n%5==0) n=n/5;
+
+        return n==1;
+    }
+};
